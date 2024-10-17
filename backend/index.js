3424a0787674b49ecdda45162b1a3d6811b63f78
amanydv860 <amanydv860@gmail.com>
2024-10-17 11:37:14 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,23 @@ app.use(express.urlencoded({ extended: true })); // For form data
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
-// Use CORS middleware
+const allowedOrigins = [
+    'http://localhost:5173',  // Local development
+    'https://wander-lust-tan.vercel.app' // Deployed frontend
+  ];
+
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    // Allow requests with no origin
+    // This can happen for example with mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 
